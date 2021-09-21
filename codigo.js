944bb6fd21d515d18edd828df07c86d11166ab3b
oscarpachecofd <91130418+oscarpachecofd@users.noreply.github.com>
2021-09-21 14:20:22 +0200

    Add files via upload

--- codigo.js
+++ codigo.js
@@ -2,3 +2,12 @@ function saludar(){
     window.alert("has pulsado un botón");
     console.log("botón ok");
 }
+
+
+function mostrarNumeros(){
+    var i = 0;
+    while(i<=5){
+        console.log(i);
+        i++;
+    }
+}
